15c887ad398e148395ab38036c8c310c95386290
junY97 <whwnsdud2024@naver.com>
2021-02-20 03:52:58 +0900

    dsad

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {HashRouter, Route} from 'react-router-dom';
 import Home from './components/Home';
 import Search from './components/Search'
 import Register from './components/Register';
@@ -9,12 +9,12 @@ import Login from './components/Login';
 class App extends Component {
   render(){
     return(
-      <BrowserRouter>
+      <HashRouter>
         <Route path="/" component={Home} exact={true}/>
         <Route path="/search" component={Search} exact={true}/>
         <Route path="/login" component={Login} exact={true}/>
         <Route path="/register" component={Register} exact={true}/>
-      </BrowserRouter>
+      </HashRouter>
     )
   }
 }
