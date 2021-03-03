750708507aabe49aeb435a6399c99fc2d998f1f2
junY97 <whwnsdud2024@naver.com>
2021-03-03 15:24:11 +0900

    debug

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,40 +4,41 @@ import H from '../css/Home.module.css';
 
 
 class Login extends Component {
-    state = {
-        inputId:'',
-        inputPs:''
-    }
-    loginApi = () => {
-        const options = {
-           method:"post",
-           body:JSON.stringify(this.state),
-           headers:{
-            'Content-Type': 'application/json'
-           }
-        }
+//     state = {
+//         inputId:'',
+//         inputPs:''
+//     }
+//     loginApi = () => {
+//         const options = {
+//            method:"post",
+//            body:JSON.stringify(this.state),
+//            headers:{
+//             'Content-Type': 'application/json'
+//            }
+//         }
     
-    return  fetch('/login',options);
+//     return  fetch('/login',options);
 
-    }
+//     }
     
-    render() {
-      return(
-      <div>
-            <div className={H.header}>
-                <div className={H.inner}>
-                    <div className={H.logo}>
-                        <div className={H.logo_text}>수산마켓</div>
-                    </div>
+//     render() {
+//       return(
+//       <div>
+//             <div className={H.header}>
+//                 <div className={H.inner}>
+//                     <div className={H.logo}>
+//                         <div className={H.logo_text}>수산마켓</div>
+//                     </div>
                     
-                </div>
-            </div>
-         아이디: <input type="text" onChange={e=>this.setState({inputId:e.target.value})}/>
-         패스워드 : <input type="password" onChange={e=>this.setState({inputPs:e.target.value})} />
-         <input type="button" value="로그인" onClick={this.loginApi}/>
-        </div>
-      )
-    }
-}
+//                 </div>
+//             </div>
+//          아이디: <input type="text" onChange={e=>this.setState({inputId:e.target.value})}/>
+//          패스워드 : <input type="password" onChange={e=>this.setState({inputPs:e.target.value})} />
+//          <input type="button" value="로그인" onClick={this.loginApi}/>
+//         </div>
+//       )
+//     }
+ }
+
 
 export default Login;
--- index.js
+++ index.js
@@ -95,27 +95,23 @@ app.post('/register', (req, res) => {
 })
 
 
-app.post('/login', (req, res) => {
-    const id = req.body.inputId;
-    const password = crypto.createHmac('sha256', key.secret).update(req.body.inputPs).digest('base64');
-    const params = [id, password];
-    const customerInfo={};
-    connection.query('select * from member where id=? and password=?', params,
-        (err, rows, field) => {
-            if(err){
-                console.log(err);
-            }
-            else{
+// app.post('/login', (req, res) => {
+//     const id = req.body.inputId;
+//     const password = crypto.createHmac('sha256', key.secret).update(req.body.inputPs).digest('base64');
+//     const params = [id, password];
+//     const customerInfo={};
+//     connection.query('select * from member where id=? and password=?', params,
+//         (err, rows, field) => {
+//             if(err){
+//                 console.log(err);
+//             }
+//             else{
         
-                console.log(rows);
-            }
-        }
-    )
-    
-    //  const token = jwt.sign({
-    //      CustomerId : 
-    //  });
-}) // => 로그인 
+//                 console.log(rows);
+//             }
+//         }
+//     )
+// }) // => 로그인 
 
 
 
